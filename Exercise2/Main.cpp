fc0de8d35b68cf57263c68a87b7ef9b1c351d173
basilije <basilije@users.noreply.github.com>
2019-03-06 15:21:00 -0800

    Update Main.cpp

--- Exercise2/Main.cpp
+++ Exercise2/Main.cpp
@@ -8,20 +8,20 @@ int main()
 	static string original_string = "Invention, my dear friends, is 93 % perspiration, 6 % electricity, 4 % evaporation, and 2 % butterscotch ripple";  // Define the original, static string
 	vector<string> new_string;  // Initializing new_string as a vector
 	vector<string>::iterator itr;  // Initializing an iterator
-	string temp_string;  // Initializing string variable
+	string temp_string;  // Initializing a string variable
 
-	new_string.clear();  // Clear the nre string
+	new_string.clear();  // Clear the new string
 
 	for (unsigned int i = size(original_string); i > 0; i--)	// Loop backward through the static string
 	{
-		temp_string = original_string[i-1];  // taking element from original string
-		new_string.push_back(temp_string);  // add that element to the end of a vector nre_string
+		temp_string = original_string[i-1];  // taking element from the original string
+		new_string.push_back(temp_string);  // add that element to the end of a vector new_string
 	}
 
 	cout << "new string contains:\n";  // print the string that explain what should be printed
 
 	for (itr = new_string.begin(); itr < new_string.end(); itr++)  // iterate a vector
-		cout << *itr;  // print vector elements
+		cout << *itr;  // print iterated vector element
 
 	return 0;
 }

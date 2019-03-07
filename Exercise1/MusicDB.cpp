e45a4b7a7fd7e62f114a3e4317df92e49eb1c897
basilije <basilije@users.noreply.github.com>
2019-03-06 18:01:50 -0800

    Add files via upload

--- Exercise1/MusicDB.cpp
+++ Exercise1/MusicDB.cpp
@@ -9,7 +9,6 @@ using namespace std;
 namespace MusicDB
 {
 
-	const int MAX_SONG_RECORDS = 8;  // define a max number of song records in database
 	string binary_database_file_name = "MyFavoriteSongs.db";  // define the database file name
 	int total_number_of_songs;  // integer to keep total number of songs in any time
 	vector<Song> all_songs; // make an vector for storing all songs
@@ -35,8 +34,7 @@ namespace MusicDB
 				try
 				{
 					file.read((char*)& the_song, sizeof(the_song));  // read the one song from the file
-					if (total_number_of_songs <= MAX_SONG_RECORDS - 1)  // if there is a space in an array
-						all_songs.push_back(the_song);  // add that song to the end ov vector
+					all_songs.push_back(the_song);  // add that song to the end ov vector
 					total_number_of_songs++;  // increase the number of total songs
 				}
 				catch (const exception & exc)
@@ -127,91 +125,86 @@ namespace MusicDB
 	{
 		string input_str;  // define the strign for taking the input
 		there_is_a_new_song = false;  // set that there is currently not a new song
+ 
+		try
+		{
+			cout << "Enter song title: ";  // print to the user what type of data to enter
+			getline(cin >> ws, input_str);  // get the input string (with spaces if needed)
 
-		if (total_number_of_songs < MAX_SONG_RECORDS - 1)  // just if there is a pre-defined space (number of songs is lower than max - 1)
-		{ 
+			if (input_str.length() > sizeof(song_to_add.song_title))  // if string is bigger than a predicted space
+				throw exception("Song title is to large! ");  // throw an exception
+			else  // in other case
+				strcpy_s(song_to_add.song_title, input_str.c_str());  // copy a string to the song_tgitle char array structure
+
+			cout << "Enter artist: ";  // print to the user what type of data to enter
+			getline(cin >> ws, input_str);  // get the input string (with spaces if needed)
+
+			if (input_str.length() > sizeof(song_to_add.artist_name))  // if string is bigger than a predicted space
+				throw exception("Artist name is to large! ");  // throw an exception
+			else  // in other case
+				strcpy_s(song_to_add.artist_name, input_str.c_str());  // copy a string to the artist_name char array structure
+
+			cout << "Enter album name: ";  // print to the user what type of data to enter
+			getline(cin >> ws, input_str);  // get the input string (with spaces if needed)
+
+			if (input_str.length() > sizeof(song_to_add.album_name))  // if string is bigger than a predicted space
+				throw exception("Album name is to large! ");  // throw an exception
+			else  // in other case
+				strcpy_s(song_to_add.album_name, input_str.c_str());  // copy a string to the album_name char array structure
+				
+			cout << "Enter track #: ";  // print to the user to enter the song's track number
+			getline(cin >> ws, input_str);  // get the input string 
 			try
 			{
-				cout << "Enter song title: ";  // print to the user what type of data to enter
-				getline(cin >> ws, input_str);  // get the input string (with spaces if needed)
-
-				if (input_str.length() > sizeof(song_to_add.song_title))  // if string is bigger than a predicted space
-					throw exception("Song title is to large! ");  // throw an exception
-				else  // in other case
-					strcpy_s(song_to_add.song_title, input_str.c_str());  // copy a string to the song_tgitle char array structure
-
-				cout << "Enter artist: ";  // print to the user what type of data to enter
-				getline(cin >> ws, input_str);  // get the input string (with spaces if needed)
-
-				if (input_str.length() > sizeof(song_to_add.artist_name))  // if string is bigger than a predicted space
-					throw exception("Artist name is to large! ");  // throw an exception
-				else  // in other case
-					strcpy_s(song_to_add.artist_name, input_str.c_str());  // copy a string to the artist_name char array structure
-
-				cout << "Enter album name: ";  // print to the user what type of data to enter
-				getline(cin >> ws, input_str);  // get the input string (with spaces if needed)
-
-				if (input_str.length() > sizeof(song_to_add.album_name))  // if string is bigger than a predicted space
-					throw exception("Album name is to large! ");  // throw an exception
-				else  // in other case
-					strcpy_s(song_to_add.album_name, input_str.c_str());  // copy a string to the album_name char array structure
-				
-				cout << "Enter track #: ";  // print to the user to enter the song's track number
-				getline(cin >> ws, input_str);  // get the input string 
-				try
-				{
-					song_to_add.track_number = stoi(input_str);  // try to convert entered string to integer and put it to the structure
-				}
-				catch (const exception &exc)  // if something went wrong
-				{
-					throw exception("Wrong track number format! ");  // throw the exeption
-					(void)exc;  // silent catch
-				}
-
-				cout << "Enter year: ";  // print to the user to enter the song's year
-				getline(cin >> ws, input_str);  // get the input string 
-				try
-				{
-					song_to_add.year_released = stoi(input_str);  // try to convert entered string to integer and put it to the structure
-				}
-				catch (const exception &exc)  // if something went wrong
-				{
-					throw exception("Wrong year format! ");  // throw the exeption
-					(void)exc;  // silent catch
-				}
-
-				cout << "Enter genre: ";  // print to the user to enter the song's genre
-				getline(cin >> ws, input_str); // get the input string 
-
-				song_to_add.song_genre = Unknown; // set genre to default value (Unknown=0) to avoid an error
-				if (input_str == "Blues")                    // other cases are obvious
-					song_to_add.song_genre = Blues;
-				else if (input_str == "Country")
-					song_to_add.song_genre = Country;
-				else if (input_str == "Electronic")
-					song_to_add.song_genre = Electronic;
-				else if (input_str == "Folk")
-					song_to_add.song_genre = Folk;
-				else if (input_str == "HipHop")
-					song_to_add.song_genre = HipHop;
-				else if (input_str == "Jazz")
-					song_to_add.song_genre = Jazz;
-				else if (input_str == "Latin")
-					song_to_add.song_genre = Latin;
-				else if (input_str == "Pop")
-					song_to_add.song_genre = Pop;
-				else if (input_str == "Rock")
-					song_to_add.song_genre = Rock;         // end of if
-
-				there_is_a_new_song = true;  // set to there is a new song entered
+				song_to_add.track_number = stoi(input_str);  // try to convert entered string to integer and put it to the structure
 			}
-			catch (const exception &exc)  // if there is an exception
+			catch (const exception &exc)  // if something went wrong
 			{
-				cout << exc.what() << endl << " Adding a new song is automatically canceled!" << endl;  // print type of exception and the information that adding is canceled
+				throw exception("Wrong track number format! ");  // throw the exeption
+				(void)exc;  // silent catch
 			}
+
+			cout << "Enter year: ";  // print to the user to enter the song's year
+			getline(cin >> ws, input_str);  // get the input string 
+			try
+			{
+				song_to_add.year_released = stoi(input_str);  // try to convert entered string to integer and put it to the structure
+			}
+			catch (const exception &exc)  // if something went wrong
+			{
+				throw exception("Wrong year format! ");  // throw the exeption
+				(void)exc;  // silent catch
+			}
+
+			cout << "Enter genre: ";  // print to the user to enter the song's genre
+			getline(cin >> ws, input_str); // get the input string 
+
+			song_to_add.song_genre = Unknown; // set genre to default value (Unknown=0) to avoid an error
+			if (input_str == "Blues")                    // other cases are obvious
+				song_to_add.song_genre = Blues;
+			else if (input_str == "Country")
+				song_to_add.song_genre = Country;
+			else if (input_str == "Electronic")
+				song_to_add.song_genre = Electronic;
+			else if (input_str == "Folk")
+				song_to_add.song_genre = Folk;
+			else if (input_str == "HipHop")
+				song_to_add.song_genre = HipHop;
+			else if (input_str == "Jazz")
+				song_to_add.song_genre = Jazz;
+			else if (input_str == "Latin")
+				song_to_add.song_genre = Latin;
+			else if (input_str == "Pop")
+				song_to_add.song_genre = Pop;
+			else if (input_str == "Rock")
+				song_to_add.song_genre = Rock;         // end of if
+
+			there_is_a_new_song = true;  // set to there is a new song entered
 		}
-		else  // if the number of songs is larger than a max number of songs defined
-			cout << "Could not add a new song, there are too many songs in the database." << endl;  // print that info to the user
+		catch (const exception &exc)  // if there is an exception
+		{
+			cout << exc.what() << endl << " Adding a new song is automatically canceled!" << endl;  // print type of exception and the information that adding is canceled
+		}
 	}
 
 	// a function that append a new song to the database file
